c1917044536c423d81817b8979f2cf12e539f6ec
Sócrates Manaure <smanaure93@gmail.com>
2022-10-21 10:02:08 -0400

    banner

--- tailwind/web-tailwind/tailwind.config.js
+++ tailwind/web-tailwind/tailwind.config.js
@@ -2,10 +2,14 @@ module.exports = {
     theme: {
         extend: {
             screens: {
-                "xs": {"max": "768px"}
+                "xs": { "max": "768px" }
             },
             backgroundImage: {
-                'body-pattern': "url('../img/pattern.png')"
+                'body-pattern': "url('../img/pattern.png')",
+                'banner': "url('../img/bakbaner.png')"
+            },
+            backgroundPosition: {
+                "banner-position": "-300px -200px"
             },
             colors: {
                 "custom-light-blue": "#37bcf9",
@@ -14,7 +18,9 @@ module.exports = {
             animation: {
                 "spin-slow": "spin 2s linear infinite",
                 "from-below": "fromBelow 500ms linear",
-                "from-right": "fromRight 500ms linear"
+                "from-right": "fromRight 500ms linear",
+                "bg-banner": "bgBanner 2s linear",
+                "text-banner": "trailerEffect 2s linear"
             },
             keyframes: {
                 fromBelow: {
@@ -26,6 +32,16 @@ module.exports = {
                     '0%': { transform: 'translateX(0%)' },
                     '50%': { transform: 'translateX(200%)' },
                     '100%': { transform: 'translateX(0%)' },
+                },
+                bgBanner: {
+                    '0%': { 'background-position': '0px 0px' },
+                    '100%': { 'background-position': '-300px -200px' },
+                },
+                trailerEffect: {
+                    '0%': { transform: 'translateX(-600%) scale(5,5)', opacity: 1 },
+                    '50%': { transform: 'translateX(600%) scale(5,5)', opacity: 1 },
+                    '75%': { transform: 'translateX(-600%) scale(5,5)', 'text-shadow': 'none', color: 'transparent', opacity: 0 },
+                    '100%': { transform: 'translateX(0%) scale(1,1)', 'text-shadow': '2px 2 px 1px black', color: 'white', opacity: 1 },
                 }
             }
         },
