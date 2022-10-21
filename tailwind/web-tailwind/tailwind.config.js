90258e8e51eab774e42be56bec75456d32d55baa
Sócrates Manaure <smanaure93@gmail.com>
2022-10-21 07:06:59 -0400

    animando logotipo

--- tailwind/web-tailwind/tailwind.config.js
+++ tailwind/web-tailwind/tailwind.config.js
@@ -7,7 +7,28 @@ module.exports = {
             colors: {
                 "custom-light-blue": "#37bcf9",
                 "custom-dark-blue": "#0370b9"
+            },
+            animation: {
+                "spin-slow": "spin 2s linear infinite",
+                "from-below": "fromBelow 500ms linear",
+                "from-right": "fromRight 500ms linear"
+            },
+            keyframes: {
+                fromBelow: {
+                    '0%': { transform: 'translateY(0%)' },
+                    '50%': { transform: 'translateY(200%)' },
+                    '100%': { transform: 'translateY(0%)' },
+                },
+                fromRight: {
+                    '0%': { transform: 'translateX(0%)' },
+                    '50%': { transform: 'translateX(200%)' },
+                    '100%': { transform: 'translateX(0%)' },
+                }
             }
-        }
+        },
+    },
+    variants: {
+        animation: ['responsive', 'hover', 'group-hover'],
+        animate: ['responsive', 'hover', 'group-hover'],
     }
 }
