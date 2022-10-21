053fd231f7ce1235874aa68b3ad92204424d1ef5
Sócrates Manaure <smanaure93@gmail.com>
2022-10-20 23:21:18 -0400

    cabecera de sitio web parte 1

---
+++ tailwind/web-tailwind/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    theme: {
+        extend: {
+            backgroundImage: {
+                'body-pattern': "url('../img/pattern.png')"
+            },
+            colors: {
+                "custom-light-blue": "#37bcf9",
+                "custom-dark-blue": "#0370b9"
+            }
+        }
+    }
+}
